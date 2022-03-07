e274feb2080647f28935173a9b55acc1d9bf46ba
Mitch Patterson <mpatterson3@hotmail.com>
2022-03-06 22:02:51 -0500

    added public folder

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.static('public'));
 app.get("/",(req,res)=>{
-    res.send("Welcome to the homepage!");
+    res.send("index.html");
 });
 app.listen(port,()=>{
     console.log("wassup on port ",port);
