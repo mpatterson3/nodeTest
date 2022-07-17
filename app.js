a6161571a88b9da3ad1f2e0f1e4a87b01cfe5a08
Mitch Patterson <mpatterson3@hotmail.com>
2022-07-17 19:22:18 -0400

    added mongoose db capabilities

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
 
 const express = require("express");
-
 const app = express();
-app.use(express.static('public'));
-const port = process.env.PORT || 3000;
 
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/loudworm');
+var db = mongoose.connection;
+db.on('connection',()=>{
+    console.log('connected to db')
+});
+const subscriberSchema = {
+    fname:String,
+    lname:String,
+    phone:String,
+    email:String
+};
+mongoose.model('subscriber',subscriberSchema);
+//app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+const port = process.env.PORT || 3000;
 
 app.get("/",(req,res)=>{
     res.send("index.html");
 });
-
+app.post('/',(req,res)=>{
+    res.send(req.body);
+    console.log(req.body);
+    mongoose.model('subscriber').create(
+        {
+            fname:req.body.fname,
+            lname:req.body.lname,
+            phone:req.body.phone,
+            email:req.body.email
+        },
+        function(err){
+            if(!err){
+                return;
+            }
+            res.send("you fucked up Mel");
+        }
+    );
+});
 app.listen(port,()=>{
     console.log("wassup on port ",port);
 });
