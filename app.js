2277e24706a2b0e353c74b81644de11bc0ebb156
Mitchell Patterson <mpatterson3@hotmail.com>
2023-02-18 15:03:10 -0500

    commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 const indexRouter = require("./routes/home");
-const subscriberRouter = require("./routes/home");
+const subscriberRouter = require("./routes/subscribers");
 const mongoose = require("mongoose");
 mongoose.connect('mongodb+srv://mpatterson3:Destiny10161971@cluster0.etaow.mongodb.net/?retryWrites=true&w=majority');
 var db = mongoose.connection;
@@ -23,7 +23,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.use("/",indexRouter);
 app.use("/subscribers",subscriberRouter);
-
+app.set('viewengine','jade')
 const port = process.env.PORT || 3000;
 
 
