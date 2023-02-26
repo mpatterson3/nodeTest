0379cc088ac6327347e47a58b03610f0ce1fe2f1
Mitchell Patterson <mpatterson3@hotmail.com>
2023-02-26 12:50:07 -0500

    Added schema

--- app.js
+++ app.js
@@ -4,21 +4,21 @@ const app = express();
 
 const indexRouter = require("./routes/index");
 const subscriberRouter = require("./routes/subscribers");
-// const mongoose = require("mongoose");
-// mongoose.connect('mongodb+srv://mpatterson3:Destiny10161971@cluster0.etaow.mongodb.net/?retryWrites=true&w=majority');
-// var db = mongoose.connection;
-// db.on("error", console.error.bind(console, "MongoDB connection error:"));
-// // db.on('connection',()=>{
-// //     console.log('connected to db');
-// // });
-// const subscriberSchema = {
-//     fname:String,
-//     lname:String,
-//     phone:String,
-//     email:String
-// };
-// mongoose.model('subscribers',subscriberSchema);
-//app.use(express.json());
+const mongoose = require("mongoose");
+mongoose.connect('mongodb+srv://mpatterson3:Destiny10161971@cluster0.etaow.mongodb.net/?retryWrites=true&w=majority');
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on('connection',()=>{
+     console.log('connected to db');
+});
+const subscriberSchema = {
+    fname:String,
+    lname:String,
+    phone:String,
+    email:String
+};
+mongoose.model('subscribers',subscriberSchema);
+app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.use("/",indexRouter);
